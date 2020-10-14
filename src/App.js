eef65f452ebca150633e559e81a9f43608aab3de
JasonPing <eazyee6@gmail.com>
2020-10-14 19:38:21 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import "./App.css";
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To My Hello World!</h1>
+    </header>
+    <p className="App-intro">My name is jason ping. Welcome, and hello!</p>
+  </div>
+);
